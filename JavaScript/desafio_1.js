24145a8dab33854d55e551e2731ba6bf4b387aa2
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-06-13 11:32:17 -0300

    Update Desafio Dígito Único

--- JavaScript/desafio_1.js
+++ JavaScript/desafio_1.js
@@ -2,7 +2,7 @@ const readlineSync = require('readline-sync');
 
 let n = readlineSync.question('Digite o valor de N: ')
 let k = readlineSync.question('Digite o valor de k: ')
-console.log(typeof n)
+
 function digitoUnico(n, k) {
 
     let digito = new Array();
